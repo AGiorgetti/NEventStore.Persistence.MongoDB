52a5a47f4c7c8617447a1d95d4e828da003173b3
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-05-06 16:31:43 +0200

    fixed the build script to embed the AssemblyInformationalVersion

--- src/VersionAssemblyInfo.cs
+++ src/VersionAssemblyInfo.cs
@@ -5,4 +5,4 @@
 // Used for deployment. You can increase this number for every deployment. It is used by setup programs.
 [assembly: AssemblyFileVersion("5.3.4.0")]
 // The Product version of the assembly. This is the version you would use when talking to customers or for display on your website (the last custom number will be replaced with the revision/commit hashtag). 
-[assembly: AssemblyInformationalVersion("5.3.4.0.8f58be32687bfbd5ee26fb3fb4989a322c75574f")]
+[assembly: AssemblyInformationalVersion("5.3.4.0.0")]
